e652af72c96b3f6766ef46618baa36ab2d23caf7
fsegatto <ferspigao@hotmail.com>
2025-11-04 03:32:30 -0300

    ✨ Add pneumonia model

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
+
+import os
 # Biblioteca para criar aplicativos web e APIs
 from flask import Flask
 # Biblioteca para simplificar e acelerar a construção de APIs RESTful. Ele adiciona um conjunto de ferramentas e decoradores para ajudar a descrever e documentar a API de forma automática
-from flask_restx import Api, Resource, fields
+from flask_restx import Api, Resource, fields, reqparse
+
+from werkzeug.datastructures import FileStorage
+
+from werkzeug.utils import secure_filename
 # Biblioteca para otimizar e acelerar fluxos de trabalho que envolvem tarefas computacionalmente intensivas
 import joblib
+
+from keras.models import load_model
+
+
+from PIL import Image
+
+import io
 # Biblioteca para trabalhar com arrays multidimensionais e matrizes de forma eficiente
 import numpy as np
 # Biblioteca para importar arquivos
@@ -13,13 +26,14 @@ from waitress import serve
 # Carregar modelo e scaler
 model = joblib.load("diabetes_model.pkl")
 scaler = joblib.load("scaler.pkl")
+pneumonia_model = load_model("pneumonia_model.h5")
 
 app = Flask(__name__)
-api = Api(app, version='1.0', title='Diabetes API',
-          description='API to check if a person has diabetes using artificial intelligence.')
+api = Api(app, version='1.0', title='Predictions API',
+          description='API to check if a person has diabetes or pneumonia using artificial intelligence.')
 
 # Define a namespace
-ns = api.namespace('Diabetes', description='Diabetes operations')
+ns = api.namespace('predictions', description='Predictions operations')
 
 # Define a model for request body
 diabetes_model = api.model('Diabetes', {
@@ -34,13 +48,27 @@ diabetes_model = api.model('Diabetes', {
 })
 
 # Define a model for response body
-prediction_model = api.model('Prediction', {
+prediction_model = api.model('DiabetesPrediction', {
     'hasDiabetes': fields.Boolean(readOnly=True, description='Express the prediction'),
     'probability': fields.String(required=True, description='Express the probability')
 })
 
+# Define a model for response body
+pneumonia_prediction_model = api.model('PneumoniaPrediction', {
+    'result': fields.String(readOnly=True, description='Express the result'),
+    'confidence': fields.String(required=True, description='Express the confidence')
+})
 
-@ns.route('/')
+upload_parser = reqparse.RequestParser()
+upload_parser.add_argument('file',
+                          location='files',
+                          type=FileStorage,
+                          required=True,
+                          help='The file to upload')
+
+IMG_SIZE = (150, 150)
+
+@ns.route('/diabetes')
 class DiabetesResource(Resource):
     @ns.doc('Check_Diabetes')
     @ns.expect(diabetes_model)
@@ -68,6 +96,42 @@ class DiabetesResource(Resource):
         }
         # Retornar dict e status — Flask-RESTX fará o marshal e a serialização
         return response_data, 200
+
+@ns.route('/pneumonia')
+class FileUpload(Resource):
+    """Handles file uploads and returns a JSON response."""
+    @api.expect(upload_parser)
+    @ns.marshal_with(pneumonia_prediction_model, code=200)
+    def post(self):
+        """Uploads a file and processes it."""
+        args = upload_parser.parse_args()
+        uploaded_file = args['file']
+
+        if uploaded_file:
+            # For security, use secure_filename() to sanitize the filename
+            filename = secure_filename(uploaded_file.filename)
+            
+            img = Image.open(io.BytesIO(uploaded_file.read())).convert("L")
+            img = img.resize(IMG_SIZE)
+            img_arr = np.expand_dims(np.array(img) / 255.0, axis=0)
+
+            # Fazer predição
+            pred = pneumonia_model.predict(img_arr)[0][0]
+            result = "NORMAL" if pred > 0.5 else "PNEUMONIA"
+            confidence = f"{(float(pred if pred > 0.5 else 1 - pred) * 100):.2f}%"
+            
+            # Return a JSON response with a success message
+            return {
+                'result': result,
+                'confidence': confidence
+            }, 200 # HTTP status code for created
+        
+        # If no file is provided, an error will be returned automatically by the parser
+        # with an appropriate error message and status code (400 Bad Request).
+        return {
+            'message': 'File upload failed',
+            'status': 'error'
+        }, 400
 
 if __name__ == "__main__":
     serve(app, host="0.0.0.0", port=8090)
